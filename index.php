8363fabfae29431b8f2711929ed117834caf86f7
PedroRuama <ruama246@gmail.com>
2023-10-20 23:23:54 -0300

    site posto em WebHost 000webhost.com

--- index.php
+++ index.php
@@ -34,7 +34,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Loja dataBase</title>
+    <title>Loja ElmaChips</title>
     <link rel="stylesheet" href="style/index.css">
 
     
